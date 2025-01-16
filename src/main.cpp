fc3e284cd7effda4a0a6cad74cfd705e249505be
Kuh456 <kudog1106@gmail.com>
2025-01-16 17:51:39 +0900

    ライブラリの追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <CAN/CAN.h>
+#include <SPICREATE.h>
+#include <SPIflash.h>
 
 #define CAN_RX 19
 #define CAN_TX 18
@@ -17,28 +19,37 @@
 #define MOSI 21
 #define SCK 27
 #define FLASH_CS 2
+#define SPI_FREQUENCY 5000000
 #define LED 13
 
 CAN_CREATE CAN;
+SPICREATE::SPICreate SPIC1;
+Flash flash1;
 
 void setup()
 {
   Serial1.begin(115200, SERIAL_8N1, TWELITE_RX_back, TWELITE_TX_back);   // 本部 18ch
-  Serial2.begin(115200, SERIAL_8N1, TWELITE_RX_front, TWELITE_TX_front); // 上部基板
+  Serial2.begin(115200, SERIAL_8N1, TWELITE_RX_front, TWELITE_TX_front); // 上部基板 26ch
   Serial.begin(115200);
   // while (!Serial);
 
   Serial1.println("COMBOARD");
 
-  CAN.setPins(CAN_RX, CAN_TX);
+  SPIC1.begin(VSPI, SCK, MISO, MOSI);
+  flash1.begin(&SPIC1, FLASH_CS, SPI_FREQUENCY);
+  pinMode(FLASH_CS, OUTPUT);
+  digitalWrite(FLASH_CS, HIGH);
+
   pinMode(LED, OUTPUT);
   digitalWrite(LED, HIGH);
 
+  CAN.setPins(CAN_RX, CAN_TX);
   // start the CAN bus at 100 kbps
   if (!CAN.begin(100E3))
   {
     Serial1.println("Starting CAN failed!");
-    while (1);
+    while (1)
+      ;
   }
 }
 
@@ -64,31 +75,6 @@ void loop()
       break;
     case PAR_ERROR:
       Serial1.println("PAR ERROR");
-      break;
-    default:
-      break;
-    }
-  }
-  if (Serial2.available())
-  {
-    char cmd_2 = Serial2.read();
-    Serial2.println(cmd_2);
-    Serial2.print("Serial2");
-    Serial.println(cmd_2);
-    // Serial1.print("Sending packet ... ");
-
-    /* CAN send */
-    uint8_t ercd = CAN.sendPacket(0x13, cmd_2);
-    switch (ercd)
-    {
-    case CAN_OK:
-      Serial2.println("done");
-      break;
-    case ACK_ERROR:
-      Serial2.println("ACK ERROR");
-      break;
-    case PAR_ERROR:
-      Serial2.println("PAR ERROR");
       break;
     default:
       break;
